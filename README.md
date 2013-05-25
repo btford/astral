4cf1fadd5187d44f9785b5e9e4db73975563b680
Brian Ford <btford@umich.edu>
2013-05-24 23:02:45 -0400

    implement stuff

--- README.md
+++ README.md
@@ -1,13 +1,46 @@
 # Astral
 
-AST tooling framework for JavaScript.
+AST tooling framework for JavaScript focused on modularity and performance.
 Inspired by llvm.
 
 ## How it Works
 
-Various "passes" are registered in Astral.
+Various [passes](https://github.com/btford/astral-pass) are registered in Astral.
 Esprima (parser) creates AST.
 Astral runs the passes in order based on their prerequisites.
+
+## Example
+
+```javascript
+var esprima = require('esprima');
+var escodegen = require('escodegen');
+
+var astral = require('astral')();
+var myPass = require('astral-pass')();
+
+myPass.name = 'myPass';
+
+myPass.
+  when({
+    // ... AST chunk
+  }).
+  when(function (chunk, info) {
+    // return true or false
+  }).
+  transform(function (chunk, info) {
+
+  });
+
+astral.register(myPass);
+
+var ast = esprima.parse('var x = 42;');
+
+var newAst = astral.run(ast);
+
+var newSrc = escodegen.generate(newAst);
+
+console.log(newSrc);
+```
 
 ## License
 MIT
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
+var deepApply = require('./lib/deep-apply');
+
 var Astral = function () {
   this._passes = {};
+  this._info = {};
 };
 
 Astral.prototype.register = function (pass) {
@@ -12,6 +15,53 @@ Astral.prototype.register = function (pass) {
 
 Astral.prototype.run = function (ast) {
 
+  this._order().forEach(function (name) {
+    this._runPass(name, ast);
+  }, this);
+
+  return ast;
+};
+
+Astral.prototype._runPass = function (name, ast) {
+  var pass = this._passes[name];
+  if (pass.type === 'info') {
+    return this._runInfoPass(pass, name);
+  }
+  if (pass.type === 'transform') {
+    return this._runTransformPass(pass, name);
+  }
+  return new Error("Expected '" + name + "' to have type of either 'info' or 'transform'");
+};
+
+Astral.prototype._runInfoPass = function (pass, ast) {
+  var info = this._info;
+  deepApply(ast, pass._matches, function (chunk) {
+    pass._doer(info, clone(chunk));
+  });
+};
+
+Astral.prototype._runTransformPass = function (pass, ast) {
+  var info = clone(this._info);
+  deepApply(ast, pass._matches, function (chunk) {
+    pass._doer(chunk, info);
+  });
+};
+
+// order the passes
+Astral.prototype._order = function (ast) {
+
+  var order = [];
+  var toOrder = Object.keys(this._passes).length;
+  var progress = false;
+
+  do {
+
+  } while (progress || toOrder.length > 0);
+
+  if (toOrder > 0) {
+    return new Error("Unable to order " + toOrder.toString());
+  }
+
   // TODO: ordering constraints
   for (var name in this._passes) {
     if (this._passes.hasOwnProperty(name)) {
@@ -19,7 +69,7 @@ Astral.prototype.run = function (ast) {
     }
   }
 
-  return ast;
+  return order;
 };
 
 module.exports = function () {
